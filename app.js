c3c9e71e10a5dcb3871149444870c5cb86faeb65
Batppam <107057754+Batppam@users.noreply.github.com>
2025-01-22 22:21:48 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+alert('Bem vindo!');
+
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador = 0;
+
+while (contador <= 9 ){
+    alert(`Contador = ${contador}`);
+    contador++;
+}
+
+while (contador >= 0 ){
+    alert(`Contador = ${contador}`);
+    contador--;
+}
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+let numero = prompt('Contagem regressiva de:');
+
+while(numero >= 0){
+    alert(`Contagem =${numero}`);
+    numero--;
+}
+
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+let numero2 = prompt('Contagem progressiva de:');
+let zero = 0;
+
+while(numero2 >= zero){
+    alert(`Contagem = ${zero}`);
+    zero++;
+}
